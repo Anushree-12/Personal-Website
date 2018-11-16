e7584337d31b9d1cddb1f4e754ec7b6969a7f5db
anushree-12 <agopal12@uw.edu>
2018-11-16 15:51:30 -0800

    Added About Me and Homepage

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 export class About extends Component {
+
     render() {
-        return <div className="container">I like React.</div>
+        return <Grid fluid>
+            <Row>
+                <div style={{ fontSize: 40, fontWeight: 'bold', color: '#960056' }}>
+                    About Me
+                        </div>
+            </Row>
+
+            <Row>
+                <div style={{ fontSize: 20, fontWeight: 'normal' }}>
+                    <p>
+                        I have been constantly involved on campus in various clubs I have joined such as Tech++, which integrates technology and fashion, and Operation Smile, fundraising for a charity helping kids with cleft/lip palate.
+                    </p>
+                    <p>
+                        I have refined my leadership style during my two years as a Resident Advisor in UW housing and by being a Teaching Assistant for a Leadership Theory class in the Foster Business School.
+                    </p>
+                    <p>
+                        Currently, I'm a Peer Advisor in the iSchool!
+                    </p>
+                </div>
+            </Row>
+        </Grid >
     }
 }
--- my-app/src/Homepage.js
+++ my-app/src/Homepage.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 export class Homepage extends Component {
     render() {
-        return <Jumbotron>Anushree Gopal</Jumbotron>
+        return <Grid fluid>
+            <Row>
+                <Col xs={12} md={6}>
+                    <Row>
+                        <div style={{ fontSize: 40, fontWeight: 'bold', color: '#960056' }}>
+                            Anushree Gopal
+                        </div>
+                    </Row>
+
+                    <Row>
+                        <div style={{ fontSize: 20, fontWeight: 'normal' }}>
+                            <p>
+                                I am a senior in Informatics with a focus in cybersecurity. Currently, I am extremely interested by in web application pen-testing and forensics!
+                                I love reading and watching movies in my free-time.
+                        </p>
+                        </div>
+                    </Row>
+
+                    <Col xs={3}>
+                        <a href='https://github.com/Anushree-12' target="_blank" style={{ textDecoration: 'none', color: '#960056', fontSize: 22 }}>
+                            GitHub
+                            </a>
+                        <br />
+                        <a href='https://www.linkedin.com/in/anushreegopal/' target="_blank" style={{ textDecoration: 'none', color: '#960056', fontSize: 22 }}>
+                            LinkedIn
+                            </a>
+                        <br />
+                        <a href='https://medium.com/@agopal12' target="_blank" style={{ textDecoration: 'none', color: '#960056', fontSize: 22, }}>
+                            Medium
+                            </a>
+                    </Col>
+
+                </Col>
+                <Col className='image' xs={12} md={6}>
+                    <img src={'./image.png'} alt={'Anushree Gopal'} />
+                </Col>
+            </Row>
+        </Grid >
     }
 }
